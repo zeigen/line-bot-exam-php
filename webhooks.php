5f00742f7d22de4dc72ee58d4ed80780efba5292
PITCHAYA YAMPRAI <s86136@scbcorp.co.th>
2018-07-18 10:50:41 +0700

    Change

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,32 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'YDcLaF0mRchn6hxXpzCRfyTtorr9TVRQSsKy4ffms1wdcdcCfktYCWOHe8AibdppRqd2E8fWEi+heLTxrTefQiTDRMBKCZ8iOxkIGoNVqWnBGe5+tjlshf6Es64E8vDg6T1uYkQBoyzMWJvwhA3lNwdB04t89/1O/w1cDnyilFU=';
+
+
+function postback ($replyToken, $messages, $access_token)
+{
+		// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+	
+}
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -25,26 +50,24 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
+			
+			postback($replyToken, $messages, $access_token);
+		}
+		
+		// Reply only when message sent joint
+		if ($event['type'] == 'join' && $event['source']['type'] == 'group') {
+			// Get text sent
+			$text = $event['source']['groupId'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
 			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
+			
+			postback($replyToken, $messages, $access_token);
 		}
 	}
 }
